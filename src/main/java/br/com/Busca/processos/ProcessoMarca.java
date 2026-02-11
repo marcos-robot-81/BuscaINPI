04d8572cda7fc8d1650ef32ca297a2eaa1b5e778
Marcos Henrique <seu-email-do-github@gmail.com>
2026-02-11 06:17:50 -0300

    new update busca no INPI

---
+++ src/main/java/br/com/Busca/processos/ProcessoMarca.java
@@ -0,0 +1,16 @@
+package br.com.Busca.processos;
+
+public class ProcessoMarca {
+    private int numnero;
+    private String codigo;
+    private String nome;
+    private String nomeRazaoSocial;
+    private String Pais;
+    private String Uf;
+    private String clase;
+    private String especificao;
+    private String estatos;
+    private String procurado;
+
+
+}
--- src/main/java/br/com/Busca/rebozitory/LerXml.java
+++ src/main/java/br/com/Busca/rebozitory/LerXml.java
@@ -1,21 +1,33 @@
 package br.com.Busca.rebozitory;
 
 import java.io.File;
+import java.util.List;
 import java.util.Scanner;
+
+import br.com.Busca.DTO.MarcaDTO;
+import br.com.Busca.processos.ProcessoMarca;
 
 public class LerXml {
     
 
     public void lerMarcaXml(String nome){
 
+        int n = 2875;
         try{
-        File arquivo = new File("arquivo/marca/RM2875.xml");
+        File arquivo = new File("arquivo/marca/RM"+n+".xml");
         Scanner sca = new Scanner(arquivo);
+        StringBuffer resposta ;
+        List<ProcessoMarca> lista;
+
+        System.out.println(nome);
 
         while(sca.hasNextLine()){
+            String linha = sca.nextLine();
 
-            System.out.println(sca.nextLine());
-
+             if(linha.toLowerCase().contains(nome.toLowerCase())){
+                System.out.println(linha);
+             }
+            
 
 
         }
@@ -24,4 +36,12 @@ public class LerXml {
             System.err.println(e);
         }
     }
+
+
+    private ProcessoMarca busca(){
+        ProcessoMarca m = new ProcessoMarca();
+        return m;
+    }
+
+
 }
